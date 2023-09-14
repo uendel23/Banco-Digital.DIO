1e766f91361c9fa2a2fb9d3e8c0665baf02b08e5
Uendel Nascimento <carinha23@gmail.com>
2023-09-14 19:17:54 -0300

    adicionando o plugin do lombock e adicionando anotação @Data as classes

--- src/Conta.java
+++ src/Conta.java
@@ -1,4 +1,6 @@
+import lombok.Data;
 
+@Data
 public abstract class Conta implements IConta {
 	
 	private static final int AGENCIA_PADRAO = 1;
@@ -29,18 +31,6 @@ public abstract class Conta implements IConta {
 	public void transferir(double valor, IConta contaDestino) {
 		this.sacar(valor);
 		contaDestino.depositar(valor);
-	}
-
-	public int getAgencia() {
-		return agencia;
-	}
-
-	public int getNumero() {
-		return numero;
-	}
-
-	public double getSaldo() {
-		return saldo;
 	}
 
 	protected void imprimirInfosComuns() {
